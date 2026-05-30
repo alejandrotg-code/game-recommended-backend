59d54ea8380cddbd378288f772271e0ca60c2694
Alejandro Tacoronte González <alejandrotacorontegnlez@gmail.com>
2026-05-30 15:31:11 +0100

    Fix CORS middleware configuration

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = FastAPI(
 # Configurar middleware de CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Cambia esto por el origen de tu frontend
+    allow_origins=["http://localhost:5173"],  
     allow_credentials=True,
     allow_methods=["GET"],
     allow_headers=["*"],
